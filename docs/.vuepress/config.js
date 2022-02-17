d81644fb7b0a7fa5b420c655ccb345c0dd3fce26
InsHomePgup <99671204@qq.com>
2022-02-17 21:25:40 +0800

    更新一个配置，输入错误

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -104,7 +104,7 @@ module.exports = {
         {
           title: 'web',
           collapsable: true,
-          children: ['npm/2021/101001']
+          children: ['web/2021/101001']
         }
 
         // {
